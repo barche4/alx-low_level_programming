d5afa07da7c36322fcda41f2b2aaba94cec8ab1d
choice <still4barchelor@gmail.com>
2022-07-02 01:15:47 +0100

    num of base 10

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main -> two alphabet
+* main -> remove letter q and e
 * Return: Always 0 (Success)
 */
 int main(void)
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main -> base numbers
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; ++n)
+		printf("%d", n);
+
+	printf("\n");
+	return (0);
+}
