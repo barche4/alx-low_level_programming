e3a286a1f4d1ddd0c55ed0f5b47b90f9083780c4
choice <still4barchelor@gmail.com>
2022-07-02 01:20:47 +0100

    numb from base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+* main -> two alphabet
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+for (n = '0'; n <= '9'; ++n)
+		putchar(n);
+
+	putchar(10);
+
+	return (0);
+}
