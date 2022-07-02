060481991b7d07f3e90e1d31822a38fc870d0e12
choice <still4barchelor@gmail.com>
2022-07-02 01:35:01 +0100

    try

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main -> 16 base numbers in lowercase 
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a = '0';
+	int b = 'a';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
