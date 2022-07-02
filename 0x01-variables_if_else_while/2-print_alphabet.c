142238166a267ce63651f988c8136404359b876b
choice <still4barchelor@gmail.com>
2022-07-02 20:04:06 +0100

    up betty style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,10 +5,13 @@
 */
 int main(void)
 {
-	char letter;
+char letter;
 
-	for (letter = 'a'; letter <= 'z'; ++letter)
-	putchar(letter);
+for (letter = 'a'; letter <= 'z'; ++letter)
+{
+    putchar(letter);
     putchar('/n');
+}
+	
 	return (0);
 }
